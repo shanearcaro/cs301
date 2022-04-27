9a01111d174a290918eddaf773ae8ff248189820
Shane Arcaro <smarcaro@gmail.com>
2022-04-27 18:37:25 -0400

    Reading csv data

---
+++ Final/python/final.py
@@ -0,0 +1,8 @@
+# Author: Shane Arcaro and Nicholas DeLello
+# Date: 4/26/2022
+# Assignment: Final Project
+
+import pandas as pd
+
+# Read the data
+data = pd.read_csv('Final/data/fetal_health-1.csv')
